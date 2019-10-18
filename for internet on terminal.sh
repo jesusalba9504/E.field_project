e059ba892def7f123bd311d66398d38122eed6e0
Jesus Alba <jesus.alba9504@gmail.com>
2019-10-18 14:03:57 -0400

    testing

---
+++ for internet on terminal.sh
@@ -0,0 +1,10 @@
+# get internet on terminal (this goes on /home/user/.bashrc)
+export http_proxy="http://jesus.alba%40fisica.uh.cu:alba2095@proxy-trab.uh.cu:3128";
+export https_proxy="http://jesus.alba%40fisica.uh.cu:alba2095@proxy-trab.uh.cu:3128";
+export HTTP_PROXY="http://jesus.alba%40fisica.uh.cu:alba2095@proxy-trab.uh.cu:3128";
+export HTTPS_PROXY="http://jesus.alba%40fisica.uh.cu:alba2095@proxy-trab.uh.cu:3128";
+export ALL_PROXY="http://jesus.alba%40fisica.uh.cu:alba2095@proxy-trab.uh.cu:3128";
+
+# get internet on terminal (this goes on /etc/apt/apt.conf)
+Acquire::http::Proxy "http://jesus.alba%40fisica.uh.cu:alba2095@proxy-trab.uh.cu:3128";
+Acquire::https::Proxy "http://jesus.alba%40fisica.uh.cu:alba2095@proxy-trab.uh.cu:3128";
