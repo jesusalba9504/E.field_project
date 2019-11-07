a9925f0d08a7e824afc2f3596c96539ca7fdc7a4
jesus-PC-lab <jesus.alba9504@gmail.com>
2019-11-07 10:29:01 -0500

    openvpn2

---
+++ openvpn config.sh
@@ -0,0 +1,11 @@
+# get internet on terminal (this goes on /home/user/.bashrc)
+export http_proxy="http://jesus.alba%40fisica.uh.cu:pass@proxy-trab.uh.cu:3128";
+export https_proxy="http://jesus.alba%40fisica.uh.cu:pass@proxy-trab.uh.cu:3128";
+export HTTP_PROXY="http://jesus.alba%40fisica.uh.cu:pass@proxy-trab.uh.cu:3128";
+export HTTPS_PROXY="http://jesus.alba%40fisica.uh.cu:pass@proxy-trab.uh.cu:3128";
+export ALL_PROXY="http://jesus.alba%40fisica.uh.cu:pass@proxy-trab.uh.cu:3128";
+
+# get internet on terminal (this goes on /etc/apt/apt.conf)
+Acquire::http::Proxy "http://jesus.alba%40fisica.uh.cu:pass@proxy-trab.uh.cu:3128";
+Acquire::https::Proxy "http://jesus.alba%40fisica.uh.cu:pass@proxy-trab.uh.cu:3128";
+prueba.
